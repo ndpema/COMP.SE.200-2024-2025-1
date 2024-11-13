02a10b4a42731f2323c3dffd38e7e29fc2bbb149
ndpema <187519691+ndpema@users.noreply.github.com>
2024-11-13 20:31:43 +0200

    Setup

---
+++ test/test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+describe('Example', function() {
+
+  it('should always pass', function() {
+    let actual = true;
+    let expected = true;
+
+    expect(actual).to.equal(expected);
+  });
+});
